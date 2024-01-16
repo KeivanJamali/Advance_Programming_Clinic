1e2c119af7f87681a4f76fceb15549161d83a305
KeipS <kj.pj.sj@gmail.com>
2024-01-16 23:24:12 +0330

    test

--- Clinic_Project/doctor.py
+++ Clinic_Project/doctor.py
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 from appointment import Appointment
 
 class Doctor(Appointment):
     def add_doctor(self):
         pass
+=======
+class Doctor:
+    def __init__(self):
+        self.appointments = []
+>>>>>>> Stashed changes
 
     def update_doctor_info(self):
         pass
